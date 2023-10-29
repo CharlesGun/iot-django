4dcfab7fa7b6c03543edb04c193367ad5b2ad7f1
Charles <charlesgunawan32@gmail.com>
2023-10-29 16:18:09 +0700

    try timezone

--- iot_backend/settings.py
+++ iot_backend/settings.py
@@ -86,9 +86,6 @@ DATABASES = {
         "PASSWORD": "s1w2o3r4d56",
         "HOST": "db.uqsoumaphqlftqgfocqd.supabase.co", #localhost
         "PORT": "5432", #default postgresql port 5432,
-        "OPTIONS": {
-            'options': '-c timezone=Asia/Jakarta',
-        },
     }
 }
 
@@ -117,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
